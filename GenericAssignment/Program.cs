204304c5225c968d9d1d4c67e01d81f6a716954d
charasullia <charanrajr713@gmail.com>
2021-09-27 21:23:55 +0530

    [charansullia]added:To Find maximum intiger

---
+++ GenericAssignment/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace GenericAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          int output=GenericComputation.FindMaxIntiger(23, 45, 10);
+          Console.WriteLine(output);
+        }
+    }
+}
